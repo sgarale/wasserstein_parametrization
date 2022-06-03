1a575630445d6123004eb42e78fb2b42b83df97c
Alessandro Sgarabottolo <sgarale@gmail.com>
2022-06-03 15:52:41 +0200

    gaussian hearthquake version bump.
    Added the PenalisedLoss module, i.e. the node giving the quantity that must be minimized.

--- gaussian.py
+++ gaussian.py
@@ -18,18 +18,34 @@ class ThetaSigmoidNetwork(nn.Module):
         return theta
 
 
-def loss(output, target):
+class PenalisedLoss(nn.Module):
+    def __init__(self, func, penalty, mu, h, nr_samples):
+        self.func = func
+        self.penalty = penalty
+        self.mu = mu
+        # self.p = p
+        self.h = h
+        self.nr_samples = nr_samples
 
-    return
 
-def train(model, x, optimizer, criterion):
+    def forward(self, theta):
+        y = self.mu.rand(self.nr_samples, 2)
+        theta_y = theta(y)
+        # integral = torch.mean(self.func(y[:, 0] + theta_y[:, 0], y[:, 1] + theta_y[:, 1]) * self.mu.density(y[:, 0], y[:, 1]))
+        integral = torch.mean(self.func(y[:, 0] + theta_y[:, 0], y[:, 1] + theta_y[:, 1]))
+        # L2_norm_theta = torch.mean(torch.pow(theta_y, 2).sum(dim=1) * self.mu.density(y[:, 0], y[:, 1])).sqrt()
+        L2_norm_theta = torch.mean(torch.pow(theta_y, 2).sum(dim=1)).sqrt()
+        # Lp_norm_theta = torch.mean(torch.pow(torch.linalg.vector_norm(theta_y, ord=self.p, dim=1), self.p)).sqrt()
+        penal_term = self.h * self.penalty(L2_norm_theta / self.h)
+        return - (integral - self.h * penal_term)
+
+
+def train(model, x, optimizer):
     model.zero_grad()
     output = model(x)
-    loss = criterion(output, None)
-    loss.backward()
+    output.backward()
     optimizer.step()
-
-    return loss, output
+    return output
 
 
 if __name__ == '__main__':
@@ -55,12 +71,12 @@ if __name__ == '__main__':
     directions = theta(x)
     print(directions)
 
-    x = torch.tensor([[1., 2.], [3., 4.]])
+    x = torch.tensor([[1., 2.], [3., 4.], [5, 6]])
     # x = torch.rand(10, 2, device=device)
     print(f"evaluation points: {x}")
     directions = theta(x)
     print(directions)
 
-    criterion = nn.MSELoss()
+    exit()
     EPOCHS = 200
     optm = torch.optim.Adam(theta.parameters(), lr=0.001)
